083ac6e95f2cb61a740e32886c793a5a9a9a003d
Sondre Fossen-Romsaas <sondrefr@gmail.com>
2019-09-30 14:27:18 +0200

    .gitignore

---
+++ src/models/superresgan.py
@@ -0,0 +1,72 @@
+from fastai.vision import *
+from fastai.vision.gan import *
+import torchvision
+from torchvision.models import vgg16_bn
+from fastai.callbacks import *
+from src.config import *
+import os
+
+base_loss = F.l1_loss
+
+class FeatureLoss(nn.Module):
+    def __init__(self, m_feat, layer_ids, layer_wgts):
+        super().__init__()
+        self.m_feat = m_feat
+        self.loss_features = [self.m_feat[i] for i in layer_ids]
+        self.hooks = hook_outputs(self.loss_features, detach=False)
+        self.wgts = layer_wgts
+        self.metric_names = ['pixel',] + [f'feat_{i}' for i in range(len(layer_ids))
+              ] + [f'gram_{i}' for i in range(len(layer_ids))]
+        
+
+    def make_features(self, x, clone=False):
+        self.m_feat(x)
+        return [(o.clone() if clone else o) for o in self.hooks.stored]
+    
+    def forward(self, input, target):
+        out_feat = self.make_features(target, clone=True)
+        in_feat = self.make_features(input)
+        self.feat_losses = [base_loss(input,target)]
+        self.feat_losses += [base_loss(f_in, f_out)*w
+                             for f_in, f_out, w in zip(in_feat, out_feat, self.wgts)]
+        self.feat_losses += [base_loss(gram_matrix(f_in), gram_matrix(f_out))*w**2 * 5e3
+                             for f_in, f_out, w in zip(in_feat, out_feat, self.wgts)]
+        self.metrics = dict(zip(self.metric_names, self.feat_losses))
+        return sum(self.feat_losses)
+    
+    def __del__(self): self.hooks.remove()
+
+def gram_matrix(x):
+    n,c,h,w = x.size()
+    x = x.view(n, c, -1)
+    return (x @ x.transpose(1,2))/(c*h*w)
+
+def predict(data, path, generator):
+    #i=0
+    
+    #names = dl.dataset.items
+    #save_path=path
+    if (os.path.isdir(path)==False): os.mkdir(path)
+    for i in range(len(data)):
+        img = open_image(data[i])
+        #preds = generator(i)
+        a, pred_img, b = generator.predict(img)
+        torchvision.utils.save_image(pred_img, path + '/img_gen_' + str(i) + '.png')
+#         for o in preds:
+#             name = names[i].split('/')[-1]
+#             o.save(path/name)
+#             i += 1
+            
+#def predict (data, path_pred, generator):
+    
+    
+def label_data_critic (org_data, gen_data, path_org, path_gen, classes):
+    
+    labeled_data = pd.DataFrame(columns=['Filenames', 'label'])
+    for i in org_data:
+        labeled_data = labeled_data.append({'Filenames': path_org + '/' + i, 'label': classes[0]}, ignore_index=True)
+    for i in gen_data:
+        labeled_data = labeled_data.append({'Filenames': path_gen + '/' + i, 'label': classes[1]}, ignore_index=True)
+    
+    return labeled_data
+        
---
+++ src/models/wgan.py
@@ -0,0 +1,67 @@
+import os
+from os import listdir
+import torchvision
+import cv2
+
+
+from fastai.vision import *
+
+def save_pred(generator, path, num_gen):
+    if (os.path.isdir(path)==False): os.mkdir(path)
+    for i in range(num_gen):
+        random_noise = torch.randn(1, 100, 1, 1, device="cuda:0")
+        mask = generator(random_noise)
+        torchvision.utils.save_image(mask[0][0], path + '/mask_gen_' + str(i) + ".jpg")
+        
+        
+def image_cleaner(path):
+    #lag imagelist
+    imagelist = listdir(path)
+    #iterer igjennom listen
+    for i in range(len(imagelist)):
+        full_img_path = path+ '/' + imagelist[i]
+        
+        img = get_img(full_img_path)
+        os.remove(full_img_path)
+        
+        if(not_blank(img)):
+            new_img = undesired_objects(img)
+            torchvision.utils.save_image(torch.from_numpy(new_img), full_img_path)
+            
+    new_imagelist = listdir(path)
+    name_fixer(path,new_imagelist, imagelist)
+    
+def get_img(img_path):
+    img = cv2.imread(img_path)
+    cv2.imshow('image', img)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return img_gray
+
+def not_blank(img):
+    is_it_blank = False
+    if(np.unique(img).size > 1):
+        is_it_blank = True
+    return is_it_blank
+
+def undesired_objects(image):
+    image = image.astype('uint8')
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(image, connectivity=4)
+    sizes = stats[:, -1]
+
+    max_label = 1
+    max_size = sizes[1]
+    for i in range(2, nb_components):
+        if sizes[i] > max_size:
+            max_label = i
+            max_size = sizes[i]
+
+    img2 = np.zeros(output.shape)
+    img2[output == max_label] = 255
+    return img2
+
+def name_fixer(path, new_list, old_list):
+    
+    for i in range(len(new_list)):
+        old_path = path + '/' + old_list[i]
+        new_path = path + '/' + new_list[i]
+        os.rename(new_path, old_path)  
